7b86e0aaa79ecd595ef0bbd841a654a47e39e5d4
JoaoPauloDonatoJ <117325355+JoaoPauloDonatoJ@users.noreply.github.com>
2023-10-16 22:39:33 -0300

    Add files via upload
    Adição da migration Usuário

---
+++ database/migrations/2023_10_17_012102_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('id_user');
+            $table->unsignedBigInteger('id_type_user');
+            $table->string('user', 15);
+            $table->string('name_user', 100);
+            $table->string('e-mail', 100);
+            $table->integer('password');
+            $table->string('rm/ra', 11);
+            $table->timestamps();
+
+            $table->foreign('id_type_user')->references('id_type_user')->on('type_of_users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
