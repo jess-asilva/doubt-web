5b2b8ce9558a64fbef3398ffd19963c387f85e39
BrendaRomano <brenda.romano@etec.sp.gov.br>
2023-11-09 20:20:55 -0300

    telaCadastroMonitor

--- resources/views/users/registerMonitor.blade.php
+++ resources/views/users/registerMonitor.blade.php
@@ -4,21 +4,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="{{ asset('css/registerMonitor.css') }}">
     <title>Cadastre-se</title>
 </head>
 
 <body>
+
+    <header>
+        <img class="logo" src="/img/LOGO-DOUBT-TCC.png" alt="Logo">
+    </header>
     
     <form action="#" method="post">
         @csrf
         <legend>CADASTRO DE MONITOR</legend>
 
-        <input type="text" name="name" placeholder="Nome"><br>
+        <input type="text" name="name" placeholder="NOME"><br>
         @error('name')
         <div class="alert alert-danger">{{ $message }}</div>
         @enderror
-        <input type="email" name="email" placeholder="E-mail"><br>
+        <input type="email" name="email" placeholder="E-MAIL"><br>
         @error('email')
         <div class="alert alert-danger">{{ $message }}</div>
         @enderror
@@ -27,12 +31,13 @@
         <div class="alert alert-danger">{{ $message }}</div>
         @enderror
        
-        <label for="Turma">Turma:</label>
+        
         <select name="Turma" id="Turma">  
-        <option value="3A">3ºA</option>  
-        <option value="3B">3ºB</option>  
-        <option value="3C">3ºC</option>  
-        <option value="3D">3ºD</option>
+            <option value="" disabled selected>Selecione a turma</option>
+            <option value="3A">3ºA</option>  
+            <option value="3B">3ºB</option>  
+            <option value="3C">3ºC</option>  
+            <option value="3D">3ºD</option>
         </select> 
 
         <p><input type="submit" value="CADASTRAR"></p>
