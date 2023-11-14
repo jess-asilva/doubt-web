95f0fa68d4947e5261d38777dd1e392ba60ba06c
JoãoPauloDonatoJ <joaopaulodonato91@gmail.com>
2023-11-13 21:54:49 -0300

    Criação de Seeders para autenticação de login

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -20,6 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'ra/rm',
         'password',
     ];
 
