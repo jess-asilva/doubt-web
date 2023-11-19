054407d2cfc6ccb373f6362a96e2d1850e4b57e1
AndRonald <ronald.cortezlouy@gmail.com>
2023-11-18 23:47:24 -0300

    Criação da tela de venda do nosso produto

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,129 @@
-<h1>Página de Apresentação</h1>
-
-
-<p>Aqui ficará a página de propaganda e compra do Doubt</p>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PROMOTION</title>
+    <link rel="stylesheet" href="../css/promotion.css">
+</head>
+<body>
+<header>
+    <nav>
+        <div>
+            <img class="header-img" src="../img/MicrosoftTeams-image.png" alt="">
+        </div>
+        <div class="header-button">
+            <a class="header-button-" href="">ENTRAR</a>
+            <a class="header-button-" href="">CADASTRAR-SE</a>
+        </div>
+    </nav>
+</header>
+<main>
+    <section>
+        <div class="section-column">
+            <div class="section-column-first">
+                <div class="section-column-phrase">
+                    <h1 >Em caso de dúvidas, não hesite, consulte o Doubt!</h1>
+                </div>
+                <div>
+                    <p class="section-column-text">O Doubt é a chave para a dificuldade em matérias do seu curso. Em momentos de crise, sempre procuramos ajuda em locais confiáveis e seguros, aqui você encontra tudo isso!</p>
+                </div>
+                <div>
+                    <a class="button-knowmore" href="">SAIBA MAIS</a>
+                </div>
+            </div>
+            <div class="section-column-second">
+                <img class="section-img" src="../img/Design sem nome(1).png
+                    " alt="">
+            </div>
+        </div>
+    </section>
+    <div class="div-text-advantages">
+        <div>
+            <p class="div-text-">Principais vantagens do sistema:</p>
+        </div>
+    </div>
+    <div class="div-description">
+        <div class="div-description-">
+            <div class="div-description-row">
+                <a class="div-description-bubbles">
+                    <img src="../img/1.png" alt="">
+                    <p>Mantenha registro dos monitores.</p>
+                </a>
+            </div>
+            <div class="div-description-row">
+                <a class="div-description-bubbles">
+                    <img src="../img/3.png" alt="">
+                    <p>Material de estudo digital.</p>
+                </a>
+            </div>
+            <div class="div-description-row">
+                <a class="div-description-bubbles">
+                    <img src="../img/2.png" alt="">
+                    <p>Fórum de duvidas 24 horas.</p>
+                </a>
+            </div>
+            <div class="div-description-row">
+                <a class="div-description-bubbles">
+                    <img src="../img/4.png" alt="">
+                    <p>Organização e  ambiente de reforço escolar</p>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="div-text-enhance-experience">
+        <div>
+            <p class="div-text-">Aprimore a experiência estudantil de seus alunos!</p>
+        </div>
+    </div>
+    <section class="section-form">
+        <div class="div-form">
+            <div>
+                <form action="">
+                    <h1 class="div-text-acquired">ADQUIRA O DOUBT:</h1>
+                    <div class="row">
+                        <div class="column">
+                            <div class="input-group">
+                                <input type="text" placeholder="Nome">
+                            </div>
+                            <div class="input-group">
+                                <input type="text" placeholder="Institution">
+                            </div>
+                            <div class="input-group">
+                                <input type="tel" placeholder="Número">
+                            </div>
+                            <div class="input-group">
+                                <input type="email" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="input-group">
+                                <textarea name="" id="" cols="30" rows="10"></textarea>
+                            </div>
+                            <div>
+                                <input type="checkbox">
+                                <a class="cheackbox-text" href="">Eu concordo em receber comunicações...</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row row_">
+                        <button class="button-enviar" id="submit">ENVIAR</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</main>
+<footer>
+    <nav>
+        <div class="footer-block">
+            <a class="footer-block-text" href="aboutus.html">Sobre nós</a>
+            <a class="footer-block-text" href="privacypolicy.html">Política de privacidade</a>
+        </div>
+        <div>
+            <img class="header-img" src="../img/MicrosoftTeams-image.png" alt="">
+        </div>
+    </nav>
+</footer>
+</body>
+</html>
