1d66afa65a8af33546910e602e8d9df752ae4f8c
Jessica Alves <jessicaasilva.jpj@gmail.com>
2023-11-22 13:45:36 -0300

    Inserção texto politica de privacidade

--- resources/views/privacyPolicy.blade.php
+++ resources/views/privacyPolicy.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Privacy Policy</title>
     <link rel="stylesheet" href="{{ asset('css/privacyPolicy.css') }}">
 </head>
+
 <body>
     <header>
         <nav>
@@ -18,14 +20,24 @@
             </div>
         </nav>
     </header>
-     <main>
+    <main>
         <div class="container">
             <div class="block">
                 <div>
                     <h1>Politica de Privacidade</h1>
                 </div>
                 <div class="content">
-                    <p>A Found considera a privacidade e a segurança como prioridades fundamentais e está empenhada em assegurar a transparência no tratamento de dados pessoais de nossos usuários e clientes. A presente Política de Privacidade estabelece os procedimentos relativos à coleta, utilização e compartilhamento de informações de clientes e demais indivíduos que acessem ou utilizem nosso site.<br>Ao utilizar nossos serviços, você está ciente de que coletaremos e empregaremos suas informações pessoais conforme descrito nesta Política, em conformidade com a Constituição Federal de 1988 (art. 5º, LXXIX; e o art. 22º, XXX – incluídos pela EC 115/2022), a Lei Geral de Proteção de Dados (LGPD, Lei Federal 13.709/2018), as disposições de defesa do consumidor da Lei Federal 8078/1990 e demais regulamentos brasileiros aplicáveis.<br>Portanto, o Doubt, na qualidade de Controladora de Dados, compromete-se a cumprir o disposto nesta Política de Privacidade:</p>
+                    <p>A Found considera a privacidade e a segurança como prioridades fundamentais e está empenhada em
+                        assegurar a transparência no tratamento de dados pessoais de nossos usuários e clientes. A
+                        presente Política de Privacidade estabelece os procedimentos relativos à coleta, utilização e
+                        compartilhamento de informações de clientes e demais indivíduos que acessem ou utilizem nosso
+                        site.<br>Ao utilizar nossos serviços, você está ciente de que coletaremos e empregaremos suas
+                        informações pessoais conforme descrito nesta Política, em conformidade com a Constituição
+                        Federal de 1988 (art. 5º, LXXIX; e o art. 22º, XXX – incluídos pela EC 115/2022), a Lei Geral de
+                        Proteção de Dados (LGPD, Lei Federal 13.709/2018), as disposições de defesa do consumidor da Lei
+                        Federal 8078/1990 e demais regulamentos brasileiros aplicáveis.<br>Portanto, o Doubt, na
+                        qualidade de Controladora de Dados, compromete-se a cumprir o disposto nesta Política de
+                        Privacidade:
                 </div>
                 <div class="content">
                     <h2>Dado e finalidade:</h2>
@@ -44,15 +56,110 @@
                     <h2>Como coletamos os seus dados?</h2>
                 </div>
                 <div class="content">
-                    <p>A coleta dos seus dados pessoais ocorre da seguinte forma:<br>É a partir do seu consentimento que tratamos os seus dados pessoais. O consentimento é a manifestação livre, informada e inequívoca pela qual você autoriza o Doubt a tratar seus dados.</p>
+                    <p>A coleta dos seus dados pessoais ocorre da seguinte forma:<br>É a partir do seu consentimento que
+                        tratamos os seus dados pessoais. O consentimento é a manifestação livre, informada e inequívoca
+                        pela qual você autoriza o Doubt a tratar seus dados.<br>Assim, em consonância com a Lei Geral de
+                        Proteção de Dados, seus dados só serão coletados, tratados e armazenados mediante prévio e
+                        expresso consentimento.<br>O seu consentimento será obtido de forma específica para cada
+                        finalidade acima descrita, evidenciando o compromisso de transparência e boa-fé do Doubt para
+                        com seus usuários/clientes, seguindo as regulações legislativas pertinentes.<br>Ao utilizar os
+                        serviços do Doubt e fornecer seus dados pessoais, você está ciente e consentindo com as
+                        disposições desta Política de Privacidade, além de conhecer seus direitos e como
+                        exercê-los.<br>É importante destacar que a revogação do consentimento para o tratamento dos
+                        dados pode implicar a impossibilidade da performance adequada de alguma funcionalidade do
+                        sistema que dependa da operação. Tais consequências serão informadas previamente.
+                </div>
+                <div class="content">
+                    <h2>Quais são os seus direitos?</h2>
+                </div>
+                <div class="content">
+                    <ul>
+                        <p>O Doubt assegura a seus usuários/clientes seus direitos de titular previstos no artigo 18 da
+                            Lei
+                            Geral de Proteção de Dados. Dessa forma, você pode, de maneira gratuita e a qualquer
+                            tempo:<br>
+                            Confirmar a existência de tratamento de dados, de maneira simplificada ou em formato claro e
+                            completo.<br>
+                            Corrigir seus dados, ao solicitar a edição, correção ou atualização destes.<br>
+                            Limitar seus dados quando desnecessários, excessivos ou tratados em desconformidade com a
+                            legislação através da anonimização, bloqueio ou eliminação.<br>
+                            Eliminar seus dados tratados a partir de seu consentimento, exceto nos casos previstos em
+                            lei.<br>
+                            Revogar seu consentimento, desautorizando o tratamento de seus dados.<br>
+                            Informar-se sobre a possibilidade de não fornecer seu consentimento e sobre as consequências
+                            da
+                            negativa.
+                    </ul>
+                </div>
+                <div class="content">
+                    <h2>Como você pode exercer seus direitos de titular?</h2>
+                </div>
+                <div class="content">
+                    <ul>
+                        <p>Para exercer seus direitos de titular, você deve entrar em contato com a equipe do Doubt
+                            através
+                            dos seguintes meios disponíveis.<br>
+                            De forma a garantir a sua correta identificação como titular dos dados pessoais objeto da
+                            solicitação, é possível que solicitemos documentos ou demais comprovações que possam
+                            comprovar
+                            sua identidade. Nessa hipótese, você será informado previamente.
+                    </ul>
+                </div>
+                <div class="content">
+                    <h2>Como e por quanto tempo seus dados serão armazenados?</h2>
+                </div>
+                <div class="content">
+                    <ul>
+                        <p>Seus dados pessoais coletados pelo Doubt serão utilizados e armazenados durante o tempo
+                            necessário para a prestação do serviço ou para que as finalidades elencadas na presente
+                            Política
+                            de Privacidade sejam atingidas, considerando os direitos dos titulares dos dados e dos
+                            controladores.<br>
+                            De modo geral, seus dados serão mantidos enquanto a relação contratual entre você e o Doubt
+                            perdurar. Findado o período de armazenamento dos dados pessoais, estes serão excluídos de
+                            nossas
+                            bases de dados ou anonimizados, ressalvadas as hipóteses legalmente previstas no artigo 16
+                            da
+                            Lei Geral de Proteção de Dados.<br>
+                            Isto é, informações pessoais sobre você que sejam imprescindíveis para o cumprimento de
+                            determinações legais, judiciais e administrativas e/ou para o exercício do direito de defesa
+                            em
+                            processos judiciais e administrativos serão mantidas, a despeito da exclusão dos demais
+                            dados.
+                    </ul>
+                </div>
+                <div class="content">
+                    <h2>O que fazemos para manter seus dados seguros?</h2>
+                </div>
+                <div class="content">
+                    <ul>
+                        <p>Para mantermos suas informações pessoais seguras, usamos ferramentas físicas, eletrônicas e
+                            gerenciais orientadas para a proteção da sua privacidade.<br>
+                            Aplicamos essas ferramentas levando em consideração a natureza dos dados pessoais coletados,
+                            o
+                            contexto e a finalidade do tratamento e os riscos que eventuais violações gerariam para os
+                            direitos e liberdades do titular dos dados coletados e tratados.
+                    </ul>
+                </div>
+                <div class="content">
+                    <h2>Entre as medidas que adotamos, destacamos as seguintes:</h2>
+                </div>
+                <div class="content">
+                    <ul>
+                        <li>Apenas pessoas autorizadas têm acesso a seus dados pessoais;</li>
+                        <li>O acesso a seus dados pessoais é feito somente após o compromisso de confidencialidade;</li>
+                        <li>Seus dados pessoais são armazenados em ambiente seguro e idôneo;</li>
+                        <li>O Doubt se compromete a adotar as melhores posturas para evitar incidentes de segurança.
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
         <div class="div-button">
             <a class="button_" href="{{ route('index') }}">VOLTAR PARA O INICIO</a>
         </div>
-     </main>
-     <footer>
+    </main>
+    <footer>
         <nav>
             <div class="footer-block">
                 <a class="footer-block-text" href="{{ route('get.aboutUs') }}">Sobre nós</a>
@@ -64,4 +171,5 @@
         </nav>
     </footer>
 </body>
+
 </html>
