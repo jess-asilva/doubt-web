4cef1c1801794199c0e2a8c134596defd03adc23
Jessica Alves <jessicaasilva.jpj@gmail.com>
2023-11-29 10:51:21 -0300

    correção envido de email

--- resources/views/emails/MessageDoubtMail.blade.php
+++ resources/views/emails/MessageDoubtMail.blade.php
@@ -3,21 +3,21 @@
 
 <x-mail::message>
 
-    Mensagem de  {{ $data['name'] }}
+    Mensagem de {{ $data['name'] }}
 
     <p>Telefone: <strong>{{ $data['telPhone'] }}</strong></p> <br>
     <p>Email: <strong>{{ $data['email'] }}</strong></p> <br>
-    <p>Instituição <strong>{{ $data['email'] }}</strong></p> <br>
+    <p>Instituição <strong>{{ $data['institution'] }}</strong></p> <br>
     <p>
         Mensagem:<br>
         {{ $data['subject'] }}
     </p>
 
 
-<x-mail::button :url="'https://br.pinterest.com/'">
-    Acesse o site clicando aqui:
-</x-mail::button>
+    <x-mail::button :url="'https://br.pinterest.com/'">
+        Acesse o site clicando aqui:
+    </x-mail::button>
 
-Obrigado,<br>
-{{ config('app.name') }}
+    Obrigado,<br>
+    {{ config('app.name') }}
 </x-Mail::message>
