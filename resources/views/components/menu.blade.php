3a09990e19fddff49065aac10d6094e561f3dec0
Jessica Alves <jessicaasilva.jpj@gmail.com>
2023-12-02 08:47:15 -0300

    feat: home responsiveness

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,43 +1,81 @@
-<aside class="sidebar">
+<aside class="sidebar" id="sidemenu">
     <header class="sidebar-header">
         <link rel="stylesheet" href="{{ asset('css/menu.css') }}">
+
+        <script>
+            document.body.onclick = function(e){
+                if(~Array.from(e.target.classList).indexOf("btnExcluir")){
+                    var indice_selecionado = parseInt(e.target.dataset.i);
+                    alert(indice_selecionado);
+                    Listar();
+                }
+            }
+
+            
+
+        </script>
     </header>
 
-    <a href="{{ route('home') }}" class="logo-button">
-        <img src="{{ asset('img/LOGO-DOUBT-TCC.png') }}" alt="Logotipo Doubt" />
-    </a>
+    <button type="button" id="sidemenu-btn">
+        <img src="{{ asset('img/icon-menu.png') }}" alt="Ícone do botão que abre o menu lateral" />
+    </button type="button">
+    
 
-    <nav class="menu-buttons">
-        <a href="{{ route('home') }}">
-            <span>POSTAGENS</span>
+    <div class="menu-wrapper">
+        <a href="{{ route('home') }}" class="logo-button">
+            <img src="{{ asset('img/LOGO-DOUBT-TCC.png') }}" alt="Logotipo Doubt" />
         </a>
 
-        <a href="{{ route('get.doubts') }}">
-            <span>DÚVIDAS</span>
-        </a>
+        <nav class="menu-buttons">
+            <a href="{{ route('home') }}">
+                <span>POSTAGENS</span>
+            </a>
 
-        @can('viewAny-user')
-        <a href="{{ route('get.students') }}">
-            <span>ALUNOS</span>
-        </a>
+            <a href="{{ route('get.doubts') }}">
+                <span>DÚVIDAS</span>
+            </a>
 
-        <a href="{{ route('get.monitors') }}">
-            <span>MONITORES</span>
-        </a>
-        @endcan
+            @can('viewAny-user')
+            <a href="{{ route('get.students') }}">
+                <span>ALUNOS</span>
+            </a>
 
-        <a href="{{ route('get.schedule') }}">
-            <span>CALENDÁRIOS</span>
-        </a>
+            <a href="{{ route('get.monitors') }}">
+                <span>MONITORES</span>
+            </a>
+            @endcan
 
-        <a href="{{ route('profile') }}">
-            <span>PERFIL</span>
-        </a>
+            <a href="{{ route('get.schedule') }}">
+                <span>CALENDÁRIOS</span>
+            </a>
 
-        <div class="line"></div>
+            <a href="{{ route('profile') }}">
+                <span>PERFIL</span>
+            </a>
 
-        <a href="{{ route('logout') }}">
-            <span>SAIR</span>
-        </a>
-    </nav>
+            <div class="line"></div>
+
+            <a href="{{ route('logout') }}">
+                <span>SAIR</span>
+            </a>
+        </nav>
+    </div>
+
+    
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const menuButton = document.getElementById('sidemenu-btn');
+            const menu = document.getElementById('sidemenu');
+
+            // Adicione um ouvinte de evento de alteração aos inputs de upload
+            menuButton.addEventListener('click', function() {
+                console.log('passando aqui')
+                if (menu.classList.contains('opened')) {
+                    menu.classList.remove('opened');
+                } else {
+                    menu.classList.add('opened');
+                }
+            });
+        });
+    </script>
 </aside>
